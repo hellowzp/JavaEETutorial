9861b96b6812820624ced527e4bd9798c90368ed
wang <zpkx.wang@gmail.com>
2015-05-29 18:21:08 +0200

    add bill payment example

--- JavaEETutorial-war/src/java/cdi/interceptor/Logged.java
+++ JavaEETutorial-war/src/java/cdi/interceptor/Logged.java
@@ -5,18 +5,19 @@
  * compliance with  the terms of the License at:
  * http://java.net/projects/javaeetutorial/pages/BerkeleyLicense
  */
-package cdi;
+package cdi.interceptor;
 
-import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Target;
-import javax.inject.Qualifier;
+import javax.interceptor.InterceptorBinding;
 
-@Qualifier
+@Inherited
+@InterceptorBinding
 @Retention(RUNTIME)
-@Target({TYPE, METHOD, FIELD, PARAMETER})
-public @interface Informal {}
+@Target({METHOD, TYPE})
+public @interface Logged {
+}
